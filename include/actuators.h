e7b0d4e06e4c3259f2db61b5c914d7dc7b04c63c
Maximusk <mehdibeniche@gmail.com>
2025-06-20 00:56:04 +0200

    [DEV] Add servo functions for hooks

--- include/actuators.h
+++ include/actuators.h
@@ -1,3 +1,6 @@
 void init_servos();
 void servo_lift();
 void servo_drop();
+void servo_mid();
+void servo_drop_l();
+void servo_drop_r();
--- src/actuators.cpp
+++ src/actuators.cpp
@@ -6,6 +6,8 @@
 #define SERVO2 18
 #define SERVO3 40
 #define SERVO4 42
+#define SERVO_L SERVO4
+#define SERVO_R SERVO1
 
 /* Buzzer */
 #define BUZZER 14
@@ -20,11 +22,25 @@ void init_servos() {
     servos.attach(SERVO4);
 }
 
+void servo_mid() {
+    servos.write(SERVO_L, 90);
+    servos.write(SERVO_R, 90);
+}
+
 void servo_lift() {
-    servos.write(SERVO4, 30);
+    servos.write(SERVO4, 160);
+    servos.write(SERVO1, 10);
 }
 
 void servo_drop() {
-    servos.write(SERVO4, 150);
+    servos.write(SERVO_L, 60);
+    servos.write(SERVO_R, 110);
 }
 
+void servo_drop_l() {
+    servos.write(SERVO_L, 60);
+}
+
+void servo_drop_r() {
+    servos.write(SERVO_R, 110);
+}
